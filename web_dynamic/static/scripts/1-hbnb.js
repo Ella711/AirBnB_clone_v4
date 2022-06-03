a90113ba13c394b7ef87bf33de57e67e39861026
Ella711 <allenro711@gmail.com>
2022-06-03 11:17:27 -0500

    [ADD] - Files needed for task2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,17 @@
 window.onload =
 
-function init () {
-  const amenitiesDict = {};
-  const checkbox = document.querySelector('.amenities .popover input');
-  checkbox.change(function () {
-    const amenity = document.querySelector(this);
-    if (amenity.is(':checked')) {
-      amenitiesDict[amenity.attr('data-name')] = amenity.attr('data-id');
-    } else if (amenity.is(':not(:checked)')) {
-      delete amenitiesDict[amenity.attr('data-name')];
-    }
-    const names = Object.keys(amenitiesDict);
-    const amenitiesH4 = document.querySelector('.amenities h4');
-    amenitiesH4.text(names.sort().join(', '));
-  });
-};
+  function init () {
+    const amenitiesDict = {};
+    const checkbox = document.querySelector('.amenities .popover input');
+    checkbox.change(function () {
+      const amenity = document.querySelector(this);
+      if (amenity.is(':checked')) {
+        amenitiesDict[amenity.attr('data-name')] = amenity.attr('data-id');
+      } else if (amenity.is(':not(:checked)')) {
+        delete amenitiesDict[amenity.attr('data-name')];
+      }
+      const names = Object.keys(amenitiesDict);
+      const amenitiesH4 = document.querySelector('.amenities h4');
+      amenitiesH4.text(names.sort().join(', '));
+    });
+  };
